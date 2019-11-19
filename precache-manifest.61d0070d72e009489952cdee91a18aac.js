7a8e0e8ae6d132b9e1617a86f6fa8f1b39df770d
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-11-19 09:50:17 +0100

    Updates

---
+++ precache-manifest.61d0070d72e009489952cdee91a18aac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3949bf7af1ece356dd4fd0ef0443248c",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "ee06f6bbf21adc2c44e4",
+    "url": "/credit-card/static/css/main.51c7295f.chunk.css"
+  },
+  {
+    "revision": "b7c3b9ba33b145b23c1e",
+    "url": "/credit-card/static/js/2.c8b3a0fa.chunk.js"
+  },
+  {
+    "revision": "ee06f6bbf21adc2c44e4",
+    "url": "/credit-card/static/js/main.051c2a7f.chunk.js"
+  },
+  {
+    "revision": "1d820d890824b8d9c609",
+    "url": "/credit-card/static/js/runtime-main.80c3ea9a.js"
+  }
+]);
